3452877c0e0a8ff399fac8a94d6e1e0d8be0b25e
luan muruk <professorluanmuruk@gmail.com>
2024-09-13 09:01:29 -0300

    alterações dia 13/09

--- app/src/main/java/com/example/aula05/MainActivity.kt
+++ app/src/main/java/com/example/aula05/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : Activity() {
 
@@ -11,8 +12,13 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_constraint)
 
-        val nome = findViewById<TextView>(R.id.nomePessoa)
+        val nome = findViewById<TextView>(R.id.nome_pessoa)
+        nome.setText("Professor Luan")
 
-        nome.setText("Luan Muruk")
+        val idade = findViewById<TextView>(R.id.idade_pessoa)
+        idade.setText("34")
+
+        val profissao = findViewById<TextView>(R.id.profissao_pessoa)
+        profissao.setText("Professor sofrido")
     }
 }
