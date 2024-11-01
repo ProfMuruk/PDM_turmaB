83c659b72302798e31ed43d9c1f362df53038289
luan muruk <professorluanmuruk@gmail.com>
2024-11-01 08:47:39 -0300

    criação do banco de dados

--- app/src/main/java/com/example/aula05/dao/PessoaDAO.kt
+++ app/src/main/java/com/example/aula05/dao/PessoaDAO.kt
@@ -1,8 +1,34 @@
 package com.example.aula05.dao
 
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import com.example.aula05.model.Pessoa
 
-class PessoaDAO {
+class PessoaDAO(context : Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
+
+    companion object{
+        private const val DATABASE_NAME = "pessoa.db"
+        private const val DATABASE_VERSION = 1
+        private const val TABLE_PESSOAS = "Pessoas"
+        private const val COLUMN_ID = "id"
+        private const val COLUMN_NAME = "nome"
+        private const val COLUMN_PROFISSAO = "profissao"
+        private const val COLUMN_ALTURA = "altura"
+        private const val COLUMN_IDADE = "idade"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val createTable = """
+            CREATE TABLE $TABLE_PESSOAS(
+                $COLUMN_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                $COLUMN_NAME TEXT NOT NULL,
+                $COLUMN_ALTURA REAL NOT NULL,
+                $COLUMN_IDADE INTEGER NOT NULL,
+                $COLUMN_PROFISSAO TEXT NOT NULL
+            )
+        """.trimIndent()
+    }
 
     fun adicionar(pessoa : Pessoa){
         pessoas.add(pessoa)
@@ -10,7 +36,5 @@ class PessoaDAO {
     fun buscarPessoas() : List<Pessoa>{
         return pessoas.toList()
     }
-    companion object{
-        private val pessoas = mutableListOf<Pessoa>()
-    }
+
 }
